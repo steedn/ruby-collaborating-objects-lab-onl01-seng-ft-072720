25715867eb8980b6934ff38fb04680cafadb0834
Nathan Steed <steedn@outlook.com>
2020-08-12 04:40:19 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,15 @@
 class MP3Importer
+  attr_reader :path
 
-  attr_accessor :path
+  def initialize(path)
+    @path = path
+  end
 
-  def initialize(file)
-    @path = file
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  end
+
+  def import
+    files.each{|f| Song.new_by_filename(f)}
   end
 end
