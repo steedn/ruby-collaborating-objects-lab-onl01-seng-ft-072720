21be31fa58f3d763ea31b317f4aeb1fcc65d1b5e
Nathan Steed <steedn@outlook.com>
2020-08-12 04:43:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -75,4 +75,8 @@ end
 def print_songs
   songs.each {|song| puts song.name}
 end
+def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
