78b7cc5797a2cf39e3364af260754adfcdead3c5
Nathan Steed <steedn@outlook.com>
2020-08-12 00:55:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,13 @@
 class Song
 
-  attr_accessor :name, :artist
-
+  attr_accessor :name
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
